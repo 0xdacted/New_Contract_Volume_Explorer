9793ec6b246a7d55472cafd231be223b8f1ec325
Red <crabmarket56@gmail.com>
2023-07-13 15:54:44 +0900

    Draft application of DB and ORM

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_aio import ASYNCIO_STRATEGY
 from classes import Base, CurrentToken, OldToken
+from datetime import datetime, timedelta
 
 load_dotenv()
 ALCHEMY_API_KEY = os.getenv("ALCHEMY_API_KEY")
@@ -21,12 +22,40 @@ engine = create_engine(
     strategy=ASYNCIO_STRATEGY
 )
 
+# Create all tables in the database which are defined by Base's subclasses
 async with engine.begin() as connection:
     await connection.run_sync(Base.metadata.create_all)
 
 Session = sessionmaker(bind=engine)
 
 w3 = Web3(Web3.HTTPProvider(f'https://eth-mainnet.alchemyapi.io/v2/{ALCHEMY_API_KEY}'))
+
+async def add_token(session, contract_address, first_seen, volume):
+    new_token = CurrentToken(contract_address=contract_address,
+                             first_seen=datetime.fromtimestamp(first_seen), volume=volume)
+    session.add(new_token)
+    await session.commit()
+
+
+async def update_token(session, contract_address, volume):
+    token = await session.query(CurrentToken).filter_by(contract_address=contract_address).first()
+    token.volume += volume
+    await session.commit()
+
+
+async def consolidate_old_tokens(session):
+    tokens = await session.query(CurrentToken).filter(CurrentToken.first_seen < datetime.now() - timedelta(hours=24)).all()
+    for token in tokens:
+        old_token = OldToken(contract_address=token.contract_address,
+                             first_seen=token.first_seen, volume=token.volume)
+        session.delete(token)
+        session.add(old_token)
+    await session.commit()
+
+
+async def was_seen_before(session, contract_address):
+    token = await session.query(OldToken).filter_by(contract_address=contract_address).first()
+    return token is not None
 
 def get_token_decimals(contract):
     try:
@@ -56,7 +85,7 @@ cache_abi = {}
 while True:
     try:
         block = w3.eth.getBlock('latest')
-        if prevBlock != block:
+        if prevBlock != block.number:
             for tx in block.transactions:
                 transaction = w3.eth.getTransaction(tx)
                 contract_address = transaction['to']
@@ -65,29 +94,35 @@ while True:
                 # Fetch ABI only if it's not in the cache
                 if contract_address not in cache_abi:
                     try:
-                        response = send_request(f'https://api.etherscan.io/api?module=contract&action=getabi&address={contract_address}&apikey={ETHERSCAN_API_KEY}')
+                        response = send_request(
+                            f'https://api.etherscan.io/api?module=contract&action=getabi&address={contract_address}&apikey={ETHERSCAN_API_KEY}')
                         abi = response['result']
                         cache_abi[contract_address] = abi
                     except Exception as e:
-                        print(f"Failed to fetch ABI for contract {contract_address}, error: {e}")
+                        print(
+                            f"Failed to fetch ABI for contract {contract_address}, error: {e}")
                         continue
 
                 # Exclude non-token/NFT transactions
-                contract = w3.eth.contract(address=contract_address, abi=cache_abi[contract_address])
+                contract = w3.eth.contract(
+                    address=contract_address, abi=cache_abi[contract_address])
 
                 try:
                     # Decode function input to get function name
                     input_data = transaction['input']
                     try:
-                        function_name, _ = contract.decode_function_input(input_data)
+                        function_name, _ = contract.decode_function_input(
+                            input_data)
                     except Exception as e:
-                        print(f"Failed to decode input data for contract {contract_address}, error: {e}")
+                        print(
+                            f"Failed to decode input data for contract {contract_address}, error: {e}")
                         function_name = None
 
                     if function_name not in ['transfer', 'transferFrom']:
                         continue
                 except Exception as e:
-                    print(f"Failed to decode input data for contract {contract_address}, error: {e}")
+                    print(
+                        f"Failed to decode input data for contract {contract_address}, error: {e}")
                     continue
 
                 # Get token decimals
@@ -96,21 +131,19 @@ while True:
                 # Adjust the value accordingly
                 value_adjusted = value / 10 ** decimals
 
-                # Record first seen time if contract is new
-                if contract_address not in contract_first_seen:
-                    contract_first_seen[contract_address] = time.time()
+                # Interact with database
+                async with Session() as session:
+                    if not await was_seen_before(session, contract_address):
+                        contract_first_seen[contract_address] = time.time()
+                        await add_token(session, contract_address, contract_first_seen[contract_address], value_adjusted)
+                    else:
+                        await update_token(session, contract_address, value_adjusted)
 
-                # Update value if contract was first seen less than 24 hours ago
-                current_time = time.time()
-                if current_time - contract_first_seen[contract_address] <= 24 * 60 * 60:
-                    volume[contract_address] += value_adjusted
+            # After processing all transactions in the block
+            async with Session() as session:
+                await consolidate_old_tokens(session)
 
-            # Delete data for contracts first seen more than 24 hours ago
-            current_time = time.time()
-            for contract in list(volume.keys()):
-                if current_time - contract_first_seen[contract] > 24 * 60 * 60:
-                    del volume[contract]
-
-            prevBlock = block
+            prevBlock = block.number
+        time.sleep(15) # sleeps for 15 seconds
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
